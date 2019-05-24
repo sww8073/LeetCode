dc7073696adf5e8f5874cb68475167ff72b32b50
Stone Warren <sww8073@rit.edu>
2019-05-24 15:23:38 -0400

    Initial Commit

---
+++ TwoSum/src/TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] numsF = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if( i == j) continue;
+                if( (nums[i] + nums[j]) == target){
+                    numsF[0] = i;
+                    numsF[1] = j;
+                }
+            }
+        }
+        return numsF;
+    }
+}
